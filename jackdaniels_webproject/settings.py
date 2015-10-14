50fca20e94e8fba99de64912df724093152aea7f
Ariel Cercado <acercado@risingtide.ph>
2015-10-14 16:33:07 +0800

    staticfiles fix

--- jackdaniels_webproject/settings.py
+++ jackdaniels_webproject/settings.py
@@ -103,11 +103,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/app/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if not os.environ.get("HOME") == "/home/acercado":
     # Parse database configuration from $DATABASE_URL
