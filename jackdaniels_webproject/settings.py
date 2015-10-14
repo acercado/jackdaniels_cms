94c2d851438cce045d4bb5e0771aa6d481552499
Ariel Cercado <acercado@risingtide.ph>
2015-10-14 15:54:06 +0800

    STATIC_URL set to /app/static/

--- jackdaniels_webproject/settings.py
+++ jackdaniels_webproject/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
