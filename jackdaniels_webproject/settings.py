df1289c53c2967ce6f519385df9367ae12e1474d
Ariel Cercado <acercado@risingtide.ph>
2015-10-14 15:50:23 +0800

    STATIC_ROOT definition and collectstatic

--- jackdaniels_webproject/settings.py
+++ jackdaniels_webproject/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 
     }
 }
 
@@ -102,9 +104,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if not os.environ.get("HOME") == "/home/acercado":
