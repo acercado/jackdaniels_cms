e369d359f471383e2dfc26db520955f0678006bd
Ariel Cercado <acercado@risingtide.ph>
2015-10-14 16:58:22 +0800

    whitenoise fix

--- jackdaniels_webproject/wsgi.py
+++ jackdaniels_webproject/wsgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jackdaniels_webproject.settings")
+
 from whitenoise.django import DjangoWhiteNoise
 
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jackdaniels_webproject.settings")
